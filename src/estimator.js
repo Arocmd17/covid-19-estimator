c622481676ff87d331363a1b0cc3a2fd1a3d0586
akanderuben17@gmail.com <rakande@ad.hazonholdings.com>
2020-04-14 16:23:55 +0100

    nineth

--- src/estimator.js
+++ src/estimator.js
@@ -1,13 +1,15 @@
+
+// Estimator
 const covid19ImpactEstimator = (data) => {
   // Destructuring the given data
   const {
-    region: { avgDailyIncomeInUSD, avgDailyIncomePopulation  },
+    region: { avgDailyIncomeInUSD, avgDailyIncomePopulation },
     periodType,
-    reportedCases
-    /* totalHospitalBeds */
+    reportedCases,
+    totalHospitalBeds
   } = data;
-
   let { timeToElapse } = data;
+  // normalize days; check for weeks and months if used
   if (periodType === 'months') {
     timeToElapse = Math.trunc(timeToElapse * 30);
   } else if (periodType === 'weeks') {
@@ -15,33 +17,47 @@ const covid19ImpactEstimator = (data) => {
   } else {
     timeToElapse = Math.trunc(timeToElapse * 1);
   }
-
-  // InfectionsByRequestedTime
-  const calculateInfectionsByElapsedTime = (currentlyInfected) => {
-    const factor = Math.trunc(timeToElapse / 3);
+  // calculate InfectionsByElapsedTime
+  let calculateInfectionsByRequestedTime = (currentlyInfected) => {
+    let factor = Math.trunc(timeToElapse / 3);
     return currentlyInfected * 2 ** factor;
   };
-  // the best case estimation
+  // calculate AvailableBeds
+  let calculateAvailableBeds = (severeCasesByRequestedTime) => {
+    let bedsAvailable = totalHospitalBeds * 0.35;
+    let result = bedsAvailable;
+    return Math.trunc(result - severeCasesByRequestedTime);
+  };
+  // calculate dollarsInFlight
+  let calculateDollarsInFlight = (infectionsByRequestedTime) => {
+    let infections = infectionsByRequestedTime * avgDailyIncomeInUSD * avgDailyIncomePopulation;
+    let result = infections / timeToElapse;
+    return Math.trunc(result);
+  };
   const impact = {};
-  // challenge 1
-  impact.currentlyInfected = reportedCases * 10;
-  impact.infectionsByRequestedTime = calculateInfectionsByElapsedTime(
-    impact.currentlyInfected
-  );
-
-
-  // the severe case estimation
   const severeImpact = {};
-  // challenge 1
+  // challenge one
+  impact.currentlyInfected = reportedCases * 10;
+  impact.infectionsByRequestedTime = calculateInfectionsByRequestedTime(impact.currentlyInfected); // the best case estimation
   severeImpact.currentlyInfected = reportedCases * 50;
-  severeImpact.infectionsByRequestedTime = calculateInfectionsByElapsedTime(
-    severeImpact.currentlyInfected
-  );
-  
+  severeImpact.infectionsByRequestedTime = calculateInfectionsByRequestedTime(severeImpact.currentlyInfected); // the severe case estimation
+  // challenge two
+  impact.severeCasesByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.15); // Round up the outcome
+  impact.hospitalBedsByRequestedTime = calculateAvailableBeds(impact.severeCasesByRequestedTime); // the best case estimation
+   severeImpact.severeCasesByRequestedTime = Math.trunc(severeImpact.infectionsByRequestedTime * 0.15); // Round up the outcome
+  severeImpact.hospitalBedsByRequestedTime = calculateAvailableBeds(severeImpact.severeCasesByRequestedTime); // the severe case estimation
+  // challenge three
+  impact.casesForICUByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.05); // Round up the outcome
+  impact.casesForVentilatorsByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.02); // Round up the outcome
+  impact.dollarsInFlight = calculateDollarsInFlight(impact.infectionsByRequestedTime); // the best case estimation
+  severeImpact.casesForICUByRequestedTime = Math.trunc(severeImpact.infectionsByRequestedTime * 0.05); // Round up the outcome
+  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(severeImpact.infectionsByRequestedTime * 0.02); // Round up the outcome
+  severeImpact.dollarsInFlight = calculateDollarsInFlight(severeImpact.infectionsByRequestedTime); // the severe case estimation
   return {
     data, // the input data you got
     impact, // your best case estimation
     severeImpact // your severe case estimation
   };
 };
+}
 export default covid19ImpactEstimator;
